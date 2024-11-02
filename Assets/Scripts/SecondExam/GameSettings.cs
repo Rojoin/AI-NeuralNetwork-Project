e2962cf23a71dc38037c7f9ac9d5d7d9897bd30c
Rojoin <nachoarrastua@gmail.com>
2024-11-02 18:34:32 -0300

    Add SaveValues

--- Assets/Scripts/SecondExam/GameSettings.cs
+++ Assets/Scripts/SecondExam/GameSettings.cs
@@ -5,15 +5,15 @@ using RojoinSaveSystem.Attributes;
 public class GameSettings : ISaveObject
 {
     SaveObjectData saveObject = new SaveObjectData();
-     public int gridSizeX = 10;
-     public int gridSizeY = 10;
-     public int turnCount = 100;
-     public int PopulationCount = 40;
-     public float GenerationDuration = 20.0f;
-     public int EliteCount = 4;
-     public float MutationChance = 0.10f;
-     public float MutationRate = 0.01f;
-    [SaveValue(8)] public int[] TESTVARIABLEARRAY;
+    [SaveValue(0)]public int gridSizeX = 10;
+    [SaveValue(1)]public int gridSizeY = 10;
+    [SaveValue(2)]public int turnCount = 100;
+    [SaveValue(3)]public int PopulationCount = 40;
+    [SaveValue(4)]public float GenerationDuration = 20.0f;
+    [SaveValue(5)]public int EliteCount = 4;
+    [SaveValue(6)]public float MutationChance = 0.10f;
+    [SaveValue(7)]public float MutationRate = 0.01f;
+    [SaveValue(8)]public int[] TESTVARIABLEARRAY;
 
     public GameSettings()
     {
