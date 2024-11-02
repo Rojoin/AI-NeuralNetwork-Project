f6d2a1a5f29b3545c759f62c53a7140b614f99e7
Rojoin <nachoarrastua@gmail.com>
2024-11-02 16:18:04 -0300

    Add SaveSystem

--- Assets/Scripts/SecondExam/GameSettings.cs
+++ Assets/Scripts/SecondExam/GameSettings.cs
@@ -1,12 +1,34 @@
-﻿[System.Serializable]
-public class GameSettings
+﻿using RojoinSaveSystem;
+using RojoinSaveSystem.Attributes;
+
+[System.Serializable]
+public class GameSettings : ISaveObject
 {
-    public int gridSizeX = 10;
-    public int gridSizeY = 10;
-    public int turnCount = 100;
-    public int PopulationCount = 40;
-    public float GenerationDuration = 20.0f;
-    public int EliteCount = 4;
-    public float MutationChance = 0.10f;
-    public float MutationRate = 0.01f;
+    SaveObjectData saveObject = new SaveObjectData();
+     public int gridSizeX = 10;
+     public int gridSizeY = 10;
+     public int turnCount = 100;
+     public int PopulationCount = 40;
+     public float GenerationDuration = 20.0f;
+     public int EliteCount = 4;
+     public float MutationChance = 0.10f;
+     public float MutationRate = 0.01f;
+    [SaveValue(8)] public int[] TESTVARIABLEARRAY;
+
+    public GameSettings()
+    {
+    }
+    public int GetID()
+    {
+        return saveObject.id;
+    }
+
+    public ISaveObject GetObject()
+    {
+        return this;
+    }
+
+    public void Save()
+    {
+    }
 }
